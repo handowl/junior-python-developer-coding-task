a1d109a6fb4010120fbfa879be940f9baccef440
Elisey <elisey.trushkov@gmail.com>
2021-05-02 21:16:47 +0700

    __init__ improved and read() added

--- basic.py
+++ basic.py
@@ -1,17 +1,23 @@
-from classes import *
+from classes import CSV, XML, JSON
 import os
 import csv
 
-def get_data(files):
+def get_data(file_names):
     result = list() #список, в котором будет храниться результат
+    
+    files = list()
+    class_dict = {'csv': CSV, 'xml': XML, 'json':JSON}
+    for file_name in file_names:
+        extension = file_name.split('.')[-1]
+        file_path = os.path.join('input_data', file_name)
+        files.append(class_dict[extension](file_path))
+    for file in files:
+        file.read()
     
     #формирование списка общих ключей (названия столбцов), "пересечение" списков
     keys = set(files[0].keys)
     for f in files:
         keys &= set(f.keys)
-    
-    #TODO
-    #create different classes. Depends on "*.extension"
     
     #нормализация данных, то есть отбор тех, которые подходят по названию столбца
     for f in files:
@@ -32,6 +38,6 @@ def write_data(data, file_name):
             writer.writerow(row.values())
 
 if __name__ == '__main__':
-    input_files = os.listdir('input_data')
-    result = get_data(input_files)
+    input_file_names = os.listdir('input_data')
+    result = get_data(input_file_names)
     write_data(result, os.path.join('output_data', 'basic_results.tsv'))
--- classes.py
+++ classes.py
@@ -7,6 +7,10 @@ import xml.etree.ElementTree as ET
 class Default: 
     data = list() #список словарей {столбец: значение, ...}
     keys = list() #список только ключей, то есть только столбцов
+    file_name = str
+    
+    def __init__(self, file_name):
+        self.file_name = file_name
     
     #функция, отбирающая только нужные столбцы и их значения
     def normalize(self, keys_list):
@@ -14,20 +18,24 @@ class Default:
         for row in self.data:
             new_row = {key:str(row[key]) for key in keys_list}
             new_data.append(new_row)
-        self.data = new_data       
+        self.data = new_data
+        
+    def read(self):
+        pass       
 
 #класс данных из CSV-файла
 class CSV(Default):
-    def __init__(self, file_name):
-        with open(file_name) as csv_file:
+        
+    def read(self):
+        with open(self.file_name) as csv_file:
             reader = csv.DictReader(csv_file)
             self.data = [i for i in reader]
             self.keys = list(self.data[0].keys())
 
 #класс данных из XML-файла
 class XML(Default):
-    def __init__(self, file_name):
-        xml_tree = ET.parse('xml_data.xml')
+    def read(self):
+        xml_tree = ET.parse(self.file_name)
         root = xml_tree.getroot()
         for obj in root.iter('objects'):
             obj_data = dict()
@@ -40,8 +48,8 @@ class XML(Default):
 
 #класс данных из JSON-файла
 class JSON(Default):
-    def __init__(self, file_name):
-        with open('json_data.json') as json_file:
+    def read(self):
+        with open(self.file_name) as json_file:
             reader = json.load(json_file)
             self.data = [i for i in reader['fields']]
             self.keys = list(self.data[0].keys())
